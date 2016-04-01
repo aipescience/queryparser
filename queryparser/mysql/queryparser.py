37874569573e6e5e6486030764ba8e08163120c1
Gal Matijevic <gmatijevic@aip.de>
2016-04-01 13:46:12 +0200

    dir structure

--- queryparser/mysql/queryparser.py
+++ queryparser/mysql/queryparser.py
@@ -1,13 +1,17 @@
-import sys
+# -*- coding: utf-8 -*-
+"""
+MySQL parser.
+
+"""
+
+__all__ = ["QueryProcessor"]
+
 import antlr4
 from antlr4.error.ErrorListener import ErrorListener
-from MySQLLexer import MySQLLexer
-from MySQLParser import MySQLParser
-from MySQLParserListener import MySQLParserListener
 
-import test_queries
-import broken_queries
-import time
+from .MySQLLexer import MySQLLexer
+from .MySQLParser import MySQLParser
+from .MySQLParserListener import MySQLParserListener
 
 
 def parse_alias(alias):
@@ -52,7 +56,6 @@ class RemoveSubqueriesListener(MySQLParserListener):
                 alias = None
             alias = parse_alias(alias)
             self.subquery_aliases.append(alias)
-            #  print('removing', ctx.getText())
             ctx.parentCtx.removeLastChild()
 
 
@@ -250,78 +253,3 @@ class QueryProcessor(object):
             self.keywords = set(keywords)
         else:
             self.syntax_errors = self.syntax_error_listener.syntax_errors
-
-
-def pretty_print(q, columns, keywords, process_time, syntax=None,
-                 show_diff=True):
-
-    print('+' + '=' * 78 + '+')
-    sq = q[0].split('\n')
-
-    proc = '\033[92m\033[1mDone\033[0m'
-    offset = [0] * len(sq)
-    if len(syntax):
-        for se in syntax:
-            rep = "\033[91m\033[1m%s\033[0m" % se[2]
-            k = se[0] - 1
-            orig = sq[k]
-            sq[k] = ((orig[:se[1] + offset[k]] + rep + '\033[100m' +
-                      orig[se[1] + len(se[2]) + offset[k]:]))
-
-            proc = '\033[91m\033[1mFailed\033[0m'
-            offset[se[0] - 1] += 19
-
-    for k, i in enumerate(sq):
-        print('|\033[100m' + i + ' ' * (78 - len(i) + offset[k]) + '\033[49m|')
-
-    if not len(syntax):
-        print('|' + ' ' * 78 + '|')
-        print('|  Columns:' + ' ' * 68 + '|')
-        for i in sorted(columns):
-            print('|\t' + i + ' ' * (71 - len(i)) + '|')
-
-        if len(keywords):
-            print('|' + ' ' * 78 + '|')
-            print('|  Keywords:' + ' ' * 67 + '|')
-            for i in sorted(keywords):
-                print('|\t' + i + ' ' * (71 - len(i)) + '|')
-
-        print('|' + ' ' * 78 + '|')
-
-        if show_diff:
-            if cols.symmetric_difference(q[1]) != set():
-                print('|  Missing columns:' + ' ' * 60 + '|')
-                for i in cols.symmetric_difference(q[1]):
-                    print('|\t' + i + ' ' * (71 - len(i)) + '|')
-                print('|' + ' ' * 78 + '|')
-                proc = '\033[91m\033[1mFailed\033[0m'
-            if keys.symmetric_difference(q[2]) != set():
-                print('|  Missing keywords:' + ' ' * 59 + '|')
-                for i in keys.symmetric_difference(q[2]):
-                    print('|\t' + i + ' ' * (71 - len(i)) + '|')
-                print('|' + ' ' * 78 + '|')
-                proc = '\033[91m\033[1mFailed\033[0m'
-    else:
-        print('|' + ' ' * 78 + '|')
-
-    tm = '%s in %.2fs' % (proc, process_time)
-    if len(syntax) == 1:
-        tm += ' - 1 syntax error'
-    elif len(syntax) > 1:
-        tm += ' - %d syntax errors' % len(syntax)
-
-    print('|  %s' % tm + ' ' * (89 - len(tm)) + '|')
-    print('|' + ' ' * 78 + '|')
-    print('+' + '-' * 78 + '+')
-    print()
-
-
-if __name__ == '__main__':
-    for q in test_queries.queries[-1:]:
-        s = time.time()
-        qp = QueryProcessor(q[0])
-        cols, keys = qp.columns, qp.keywords
-
-        s = time.time() - s
-        
-        pretty_print(q, cols, keys, s, qp.syntax_errors, show_diff=True)
---
+++ queryparser/test/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ["test_queries", "broken_queries"]
