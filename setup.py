a3a89bee9142ca5b8e0ff550ded477e86c116a6a
Jochen Klar <jochenklar@gmail.com>
2017-08-07 20:48:52 +0200

    add build_py class

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-import sys
+import os
+import shutil
 import subprocess
+import sys
 import re
 
 from distutils.command.build_py import build_py as distutils_build_py
@@ -9,32 +11,25 @@ version = '0.1.0'
 
 python_version, _, _, _, _ = sys.version_info
 
-# def get_java_version():
-#     try:
-#         cmd = """java -version 2>&1 | awk '/version/ {print $3}'"""
-#         output = subprocess.check_output(cmd,  shell=True)
-
-#         return  output.decode('utf-8').strip('\n').strip('"')
-#     except OSError:
-#         return []
-
-# def find_antlr():
-#     dirs = ['/usr/local/lib', '/usr/local/bin']
-#     for d in dirs:
-#         p = os.path.join(d, 'antlr-4.7-complete.jar')
-#         if os.path.exists(p):
-#             return p
-
-
 
 class build_py(distutils_build_py):
 
-    def run(self):
+    antlr_jar = 'antlr-4.7-complete.jar'
+    antlr_dirs = ('/usr/local/lib/', '/usr/local/bin/')
 
+    def run(self):
         if self._get_java_version() < 7:
             raise RuntimeError('You need a newer version of Java.')
 
-        super(build_py, self).run()
+        antlr_path = self._get_antlr_path()
+        if not antlr_path:
+            raise RuntimeError('You need Antlr 4.7 installed in %s.' % ':'.join(self.antlr_dirs))
+
+        self._build_mysql_parser(antlr_path)
+        self._build_adql_translator(antlr_path)
+
+        # regular build, aka. copy the files into the build directory
+        distutils_build_py.run(self)
 
     def _get_java_version(self):
         try:
@@ -47,6 +42,33 @@ class build_py(distutils_build_py):
             pass
 
         raise RuntimeError('No java version found.')
+
+    def _get_antlr_path(self):
+        for directory in self.antlr_dirs:
+            path = os.path.join(directory, self.antlr_jar)
+            if os.path.exists(path):
+                return path
+        return False
+
+    def _build_mysql_parser(self, antlr_path):
+        args = ['java', '-jar', antlr_path, '-Dlanguage=Python%d' % python_version,
+            '-lib', 'queryparser/mysql']
+
+        print('building mysql lexer')
+        subprocess.call(args + ['queryparser/mysql/MySQLLexer.g4'])
+
+        print('building mysql parser')
+        subprocess.call(args + ['queryparser/mysql/MySQLParser.g4'])
+
+    def _build_adql_translator(self, antlr_path):
+        args = ['java', '-jar', antlr_path, '-Dlanguage=Python%d' % python_version,
+            '-visitor', '-lib', 'queryparser/adql']
+
+        print('building adql lexer')
+        subprocess.call(args + ['queryparser/adql/ADQLLexer.g4'])
+
+        print('building adql parser')
+        subprocess.call(args + ['queryparser/adql/ADQLParser.g4'])
 
 setup(
     name='queryparser',
@@ -74,50 +96,3 @@ setup(
         'build_py': build_py
     }
 )
-
-# def find_antlr():
-#     dirs = ['/usr/local/lib', '/usr/local/bin']
-#     for d in dirs:
-#         p = os.path.join(d, 'antlr-4.7-complete.jar')
-#         if os.path.exists(p):
-#             return p
-
-
-# def build_mysql_parser(antlr_path, major):
-#     ca = ['java', '-jar', antlr_path, '-Dlanguage=Python%d' % major, '-lib',
-#           'queryparser/mysql']
-#     print('building mysql lexer')
-#     subprocess.call(ca + ["./queryparser/mysql/MySQLLexer.g4"])
-#     print('building mysql parser')
-#     subprocess.call(ca + ["./queryparser/mysql/MySQLParser.g4"])
-
-
-# def build_adql_translator(antlr_path, major):
-#     ca = ['java', '-jar', antlr_path, '-Dlanguage=Python%d' % major,
-#           '-visitor', '-lib', 'queryparser/adql']
-#     print('building adql lexer')
-#     subprocess.call(ca + ["./queryparser/adql/ADQLLexer.g4"])
-#     print('building adql parser')
-#     subprocess.call(ca + ["./queryparser/adql/ADQLParser.g4"])
-
-
-# jv = get_java_version()
-
-# if not len(jv):
-#     raise RuntimeError("No Java found.")
-
-# try:
-#     if int(jv.split('.')[1]) < 7:
-#         raise RuntimeError("You need a newer version of Java.")
-# except (AttributeError, IndexError):
-#     print('No Java found.')
-#     raise
-
-# antlr_path = find_antlr()
-# if antlr_path is None:
-#     raise RuntimeError("Please download Antlr4.")
-
-# major, _, _, _, _ = sys.version_info
-
-# build_mysql_parser(antlr_path, major)
-# build_adql_translator(antlr_path, major)
