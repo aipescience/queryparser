cf3a9f42b3a2e4c7e37606b999abf6e809de116d
Jochen Klar <jklar@aip.de>
2017-08-24 11:31:56 +0200

    refactor setup.py and update version

--- setup.py
+++ setup.py
@@ -2,48 +2,44 @@ import sys
 
 from setuptools import setup
 
-version = '0.1.2'
+sys.path.append('src')
+
+from queryparser import __title__, __email__, __version__, __author__, __license__
 
 python_version = sys.version_info.major
 
-author = u'Gal Matijevic'
-author_email = u'gmatijevic@aip.de'
-
-packages = [
-    'queryparser',
-    'queryparser.adql',
-    'queryparser.mysql'
-]
-
-package_dir = {
-    '': 'lib/python%d' % python_version
-}
+name = '%s-python%d' % (__title__ ,python_version)
 
 requirements = [
     'pytest',
     'coverage',
     'antlr4_python%d_runtime' % python_version
 ]
-print (requirements)
+
 # work around for python 3.4 and antlr4-python3-runtime
 if sys.version_info.major == 3 and sys.version_info.minor < 5:
     requirements += ['typing']
 
-classifiers = []
-
 setup(
-    name='queryparser-python%d' % python_version,
-    version=version,
-    description='Parses MySQL and translates ADQL to MySQL.',
-    long_description=open('README.rst').read(),
+    name=name,
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    maintainer=__author__,
+    maintainer_email=__email__,
+    license=__license__,
     url='https://github.com/aipescience/queryparser',
-    author=author,
-    author_email=author_email,
-    maintainer=author,
-    maintainer_email=author_email,
-    license=u'Apache License (2.0)',
-    packages=packages,
+    description=u'Parses MySQL and translates ADQL to MySQL.',
+    long_description=open('README.rst').read(),
     install_requires=requirements,
-    classifiers=classifiers,
-    package_dir=package_dir,
+    classifiers=[],
+    packages=[
+        'queryparser',
+        'queryparser.adql',
+        'queryparser.mysql'
+    ],
+    package_dir={
+        '': 'lib/python%d' % python_version
+    },
+    include_package_data=True
 )
