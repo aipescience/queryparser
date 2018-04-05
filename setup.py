6fe6005dc2a2892581c41af870599aeb93e42491
Jochen Klar <jklar@aip.de>
2018-04-05 13:00:00 +0200

    Update version and refactor setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
-
+import re
 import sys
 
 from setuptools import setup
 
 python_version = sys.version_info.major
 
-version = '0.2.6'
-
-author = 'Gal Matijevic'
-email = 'gmatijevic@aip.de'
+with open('lib/python%d/queryparser/__init__.py' % python_version) as f:
+    metadata = dict(re.findall(r'__(.*)__ = [\']([^\']*)[\']', f.read()))
 
 requirements = [
     'pytest',
@@ -23,12 +21,12 @@ if sys.version_info.major == 3 and sys.version_info.minor < 5:
 
 setup(
     name='queryparser-python%d' % python_version,
-    version=version,
-    author=author,
-    author_email=email,
-    maintainer=author,
-    maintainer_email=email,
-    license='Apache-2.0',
+    version=metadata['version'],
+    author=metadata['author'],
+    author_email=metadata['email'],
+    maintainer=metadata['author'],
+    maintainer_email=metadata['email'],
+    license=metadata['license'],
     url='https://github.com/aipescience/queryparser',
     description=u'Parses MySQL and translates ADQL to MySQL.',
     long_description=open('README.rst').read(),
--- src/queryparser/__init__.py
+++ src/queryparser/__init__.py
@@ -1,3 +1,12 @@
+__title__ = 'queryparser'
+__version__ = '0.2.7'
+__author__ = 'Gal Matijevic'
+__email__ = 'gmatijevic@aip.de'
+__license__ = 'Apache-2.0'
+__copyright__ = 'Copyright 2016-2017 Leibniz Institute for Astrophysics Potsdam (AIP)'
+
+VERSION = __version__
+
 from . import adql
 from . import mysql
 from . import postgresql
