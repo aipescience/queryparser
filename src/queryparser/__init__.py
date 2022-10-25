b379b9f2a5d8344f8cdb110d5d30c445605c5927
Kirill Makan <kmakan@aip.de>
2022-10-25 16:41:12 +0200

    update to antlr 4.11.1

--- src/queryparser/__init__.py
+++ src/queryparser/__init__.py
@@ -1,9 +1,9 @@
 __title__ = 'queryparser'
-__version__ = '0.5.0'
+__version__ = '4.11.1'
 __author__ = 'Gal Matijevic'
 __email__ = 'gmatijevic@aip.de'
 __license__ = 'Apache-2.0'
-__copyright__ = 'Copyright 2016-2020 Leibniz Institute for Astrophysics' +\
+__copyright__ = 'Copyright 2016-2022 Leibniz Institute for Astrophysics' +\
         'Potsdam (AIP)'
 
 VERSION = __version__
