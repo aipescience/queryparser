e192bb6d9e4de7104ac20c57ed3f014d98647aa9
kimakan <45099849+kimakan@users.noreply.github.com>
2024-05-16 13:24:58 +0200

    change setup.py to pyproject.toml config

--- src/queryparser/__init__.py
+++ src/queryparser/__init__.py
@@ -1,11 +1,1 @@
-__title__ = 'queryparser'
 __version__ = '0.7.0'
-__author__ = 'Gal Matijevic'
-__email__ = 'gmatijevic@aip.de'
-__maintainer__ = 'Kirill Makan'
-__maintainer_email__ = 'kmakan@aip.de'
-__license__ = 'Apache-2.0'
-__copyright__ = 'Copyright 2016-2024 Leibniz Institute for Astrophysics' +\
-        'Potsdam (AIP)'
-
-VERSION = __version__
