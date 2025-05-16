08d2437f178382f9f5a81085e029e2b9fb092532
simeonreusch <mail@simeonreusch.com>
2025-05-16 16:41:25 +0200

    fix test

--- src/queryparser/testing/utils.py
+++ src/queryparser/testing/utils.py
@@ -37,24 +37,27 @@ def _test_parsing(query_processor, test, translate=False):
         elif query_processor == PostgreSQLQueryProcessor:
             query = adt.to_postgresql()
 
-    if replace_schema_name is None:
-        qp = query_processor(query)
-    else:
-        qp = query_processor()
-        qp.set_query(query)
-        qp.process_query(replace_schema_name=replace_schema_name)
+    if replace_function_names is None:
+        replace_function_names = []
+
+    qp = query_processor()
+    qp.set_query(query)
+    qp.process_query(
+        replace_schema_name=replace_schema_name,
+        replace_function_names=replace_function_names,
+    )
 
     qp_columns = [
-        ".".join([str(j) for j in i[:3]])
+        '.'.join([str(j) for j in i[:3]])
         for i in qp.columns
         if i[0] is not None and i[1] is not None
     ]
     qp_display_columns = [
-        "%s: %s" % (str(i[0]), ".".join([str(j) for j in i[1]]))
+        '%s: %s' % (str(i[0]), '.'.join([str(j) for j in i[1]]))
         for i in qp.display_columns
     ]
     qp_tables = [
-        ".".join([str(j) for j in i])
+        '.'.join([str(j) for j in i])
         for i in qp.tables
         if i[0] is not None and i[1] is not None
     ]
