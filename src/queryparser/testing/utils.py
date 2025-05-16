ec3c7a8ca9553a3f3869e3b71da809e19a85e5bf
simeonreusch <mail@simeonreusch.com>
2025-05-16 16:27:08 +0200

    add test for udf

--- src/queryparser/testing/utils.py
+++ src/queryparser/testing/utils.py
@@ -6,12 +6,28 @@ from queryparser.postgresql import PostgreSQLQueryProcessor
 
 
 def _test_parsing(query_processor, test, translate=False):
-    if len(test) == 6:
-        query, columns, keywords, functions, display_columns, tables = test
+    if len(test) == 7:
+        (
+            query,
+            columns,
+            keywords,
+            functions,
+            display_columns,
+            tables,
+            replace_function_names,
+        ) = test
         replace_schema_name = None
-    elif len(test) == 7:
-        query, columns, keywords, functions, display_columns, tables,\
-                replace_schema_name = test
+    elif len(test) == 8:
+        (
+            query,
+            columns,
+            keywords,
+            functions,
+            display_columns,
+            tables,
+            replace_schema_name,
+            replace_function_names,
+        ) = test
 
     if translate:
         adt = ADQLQueryTranslator()
@@ -28,13 +44,20 @@ def _test_parsing(query_processor, test, translate=False):
         qp.set_query(query)
         qp.process_query(replace_schema_name=replace_schema_name)
 
-    qp_columns = ['.'.join([str(j) for j in i[:3]]) for i in qp.columns
-                  if i[0] is not None and i[1] is not None]
-    qp_display_columns = ['%s: %s' % (str(i[0]),
-                                      '.'.join([str(j) for j in i[1]]))
-                          for i in qp.display_columns]
-    qp_tables = ['.'.join([str(j) for j in i]) for i in qp.tables
-                 if i[0] is not None and i[1] is not None]
+    qp_columns = [
+        ".".join([str(j) for j in i[:3]])
+        for i in qp.columns
+        if i[0] is not None and i[1] is not None
+    ]
+    qp_display_columns = [
+        "%s: %s" % (str(i[0]), ".".join([str(j) for j in i[1]]))
+        for i in qp.display_columns
+    ]
+    qp_tables = [
+        ".".join([str(j) for j in i])
+        for i in qp.tables
+        if i[0] is not None and i[1] is not None
+    ]
 
     if columns is not None:
         assert set(columns) == set(qp_columns)
@@ -50,4 +73,3 @@ def _test_parsing(query_processor, test, translate=False):
 
     if tables is not None:
         assert set(tables) == set(qp_tables)
-
